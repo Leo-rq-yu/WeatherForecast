8a7dc79ff8f71c2ca0375a16a7bb5db3e3419bff
LEO4388 <ruiqing6.illinois.edu>
2023-06-19 21:14:05 -0500

    debug

--- flask_docker/app.py
+++ flask_docker/app.py
@@ -144,5 +144,4 @@ def POST_weather():
     return jsonify(report), status_code
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=8080)
